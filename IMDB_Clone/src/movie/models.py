06567f5e61e964453ac75f8cd7ba6c222399c946
Manish Kumar Shah <mkshah141@gmail.com>
2020-07-20 21:35:24 +0530

    15. Added Movies Year

--- IMDB_Clone/src/movie/models.py
+++ IMDB_Clone/src/movie/models.py
@@ -3,35 +3,36 @@ from django.db import models
 # Create your models here.
 
 CATEGORY_CHOICES = (
-    ('action', 'ACTION'),
-    ('drama', 'DRAMA'),
-    ('comedy', 'COMEDY'),
-    ('romance', 'ROMANCE'),
+    ("action", "ACTION"),
+    ("drama", "DRAMA"),
+    ("comedy", "COMEDY"),
+    ("romance", "ROMANCE"),
 )
 
 LANGUAGES_CHOICES = (
-    ('EN','ENGLISH'),
-    ('GR','GERMAN'),
+    ("english", "ENGLISH"),
+    ("german", "GERMAN"),
 )
 
 STATUS_CHOICES = (
-    ('RA','RECENTLY ADDED'),
-    ('MW','MOST WATCHED'),
-    ('TR','TOP RATED'),
+    ("RA", "RECENTLY ADDED"),
+    ("MW", "MOST WATCHED"),
+    ("TR", "TOP RATED"),
 )
 
 LINK_CHOICES = (
-    ('D','DOWNLOAD LINK'),
-    ('W','WATCH LINK'),
+    ("D", "DOWNLOAD LINK"),
+    ("W", "WATCH LINK"),
 )
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
-    image = models.ImageField(upload_to='movies')
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length = 10)
-    language = models.CharField(choices=LANGUAGES_CHOICES,max_length=2)
-    status = models.CharField(choices=STATUS_CHOICES,max_length=2)
+    image = models.ImageField(upload_to="movies")
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=10)
+    language = models.CharField(choices=LANGUAGES_CHOICES, max_length=10)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=2)
     cast = models.CharField(max_length=100)
     year_of_production = models.DateField()
     views_count = models.IntegerField(default=0)
@@ -39,8 +40,11 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
+
 class MovieLinks(models.Model):
-    movie = models.ForeignKey(Movie, related_name='movie_watch_link', on_delete=models.CASCADE)
+    movie = models.ForeignKey(
+        Movie, related_name="movie_watch_link", on_delete=models.CASCADE
+    )
     type = models.CharField(choices=LINK_CHOICES, max_length=1)
     link = models.URLField()
 
